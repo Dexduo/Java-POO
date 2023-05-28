ff20e6e3387c8d834423e4a1bd8765bb27aeef2f
José Eduardo Nogueira <eduardosoaresn3@gmail.com>
2023-05-28 19:42:05 -0300

    Eighth Class

---
+++ CursoPOO Part 5/Main.java
@@ -0,0 +1,19 @@
+public class Main{
+
+    public static void main(String[] args){
+
+        //Boas praticas
+        //como escrever um nome de variavel
+            // String $1987; esse nome tbm é valido assim como "_______" ou "$$" também são nomes válidos
+            // isso aumenta a possibilidades de deixar uma variavel legivel ou indicando mais seu significado
+        //dicas
+        //casting ou coerção
+        //underline entre numeros ex.: int a = 23_456_786 para ficar mais legível
+        //palavras reservadas
+        //garbage collector (coletor de lixo)
+        //var args
+        //Herança VS Acoplamento
+
+    }
+
+}
