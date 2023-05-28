f655bf9147890a1aa0ab3613884015165bf679d5
José Eduardo Nogueira <eduardosoaresn3@gmail.com>
2023-05-28 13:09:39 -0300

    First Class

---
+++ CursoPOO/Main.java
@@ -0,0 +1,19 @@
+public class Main{
+
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+
+        byte variavelByte = -127;
+        System.out.println(variavelByte);
+
+        //short -32768 a 32767
+        //char 0 a 65535
+        //int -2bi a 2bi
+        //long -9tri a 9tri
+
+        int variavelInt = 2940123;
+        System.out.println(variavelInt);
+
+    }
+
+}
