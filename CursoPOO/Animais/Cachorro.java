a02e4db62b54136f9f6c408180595e76d9fcae95
José Eduardo Nogueira <eduardosoaresn3@gmail.com>
2023-05-28 13:28:14 -0300

    Second Class

---
+++ CursoPOO/Animais/Cachorro.java
@@ -0,0 +1,21 @@
+package Animais;
+
+public class Cachorro{
+
+    public String nome;
+    public String cor;
+    public int altura;
+    public double peso;
+    public int tamanhoDoRabo;
+
+    public void comer(){
+
+    }
+
+    public void latir(){
+        System.out.println("AU AU");
+    }
+
+    
+
+}
--- CursoPOO/Main.java
+++ CursoPOO/Main.java
@@ -1,18 +1,40 @@
+import Animais.Cachorro;
+
 public class Main{
 
     public static void main(String[] args){
-        System.out.println("Hello World!");
+        
+        // Cachorro cachorro1;
+        // cachorro1 = new Cachorro();
 
-        byte variavelByte = -127;
-        System.out.println(variavelByte);
+        Cachorro cachorro1 = new Cachorro();
 
-        //short -32768 a 32767
-        //char 0 a 65535
-        //int -2bi a 2bi
-        //long -9tri a 9tri
+        cachorro1.nome = "Puppy";
+        cachorro1.cor = "Marron";
+        cachorro1.altura = 25;
+        cachorro1.peso = 5.5;
+        cachorro1.tamanhoDoRabo = 5;
 
-        int variavelInt = 2940123;
-        System.out.println(variavelInt);
+        // System.out.println(cachorro1);
+
+        cachorro1.latir();
+
+        
+
+        //+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+
+        // System.out.println("Hello World!");
+
+        // byte variavelByte = -127;
+        // System.out.println(variavelByte);
+
+        // //short -32768 a 32767
+        // //char 0 a 65535
+        // //int -2bi a 2bi
+        // //long -9tri a 9tri
+
+        // int variavelInt = 2940123;
+        // System.out.println(variavelInt);
 
     }
 
